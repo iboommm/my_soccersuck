9f9f9ba6e906da8510689df4060cada50fc3248b
iboommm <iboommm@live.com>
2019-07-21 00:08:49 +0700

    Apply style and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { BoardComponent } from './board/board.component';
+import { TopicComponent } from './topic/topic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "board/:id", component: BoardComponent },
+  { path: "topic/:id", component: TopicComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
